5301353879c05197767239f129cf07dcede16332
Panlo62 <pankaj6255dav@gmail.com>
2023-10-12 12:30:18 +0530

    Add comments

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ const pokemonList = document.querySelector("#pokemonList");
 const buttonsHeader = document.querySelectorAll(".btn-header");
 const buttonShowMore = document.querySelector(".btn-more");
 const URL = "https://pokeapi.co/api/v2/pokemon/";
-const numPokemon = 1016;
-let cancelButtons = [];
+const numPokemon = 1016; //Number of pokemon data available in through pokeAPI
+let cancelButtons = []; //Buttons selected by user
 let pokemonCards = [];
-let tagList = [];
-let start = 1;
+let tagList = []; //Tags selected by the user
+let curr = 1; //The current number of pokemon which needs to taken from pokeAPI
 
 function getData(initial, final) {
   for (let i = initial; i < final && i <= numPokemon; i++) {
@@ -14,7 +14,7 @@ function getData(initial, final) {
       .then((response) => response.json())
       .then((data) => pokemonMonster(data));
   }
-  start = final;
+  curr = final; //Changing the value of pokemon to be taken next
 }
 
 function pokemonMonster(poke) {
@@ -30,6 +30,7 @@ function pokemonMonster(poke) {
     pokeId = "0" + pokeId;
   }
 
+  //Adding the pokemon card
   const div = document.createElement("div");
   div.classList.add("pokemon");
   div.innerHTML = `
@@ -53,43 +54,48 @@ function pokemonMonster(poke) {
     `;
   pokemonList.append(div);
 
-  if (tagList.length !== 0) noneDisplay(div);
+  //To display a particular card or not based on the selected tags
+  if (tagList.length !== 0) display(div);
 }
 
-function noneDisplay(pokemon) {
-  const types = pokemon.querySelector(".pokemon-types").innerHTML;
+function display(pokemon) {
+  const types = pokemon.querySelector(".pokemon-types").innerHTML; //Types of a pokemon
 
   if (tagList.length === 0) {
-    pokemon.style.display = "block";
+    pokemon.style.display = "block"; //All cards displayed
   } else if (
     (tagList.length === 1 && !types.includes(tagList[0])) ||
     (tagList.length === 2 &&
       (!types.includes(tagList[0]) || !types.includes(tagList[1])))
   ) {
-    pokemon.style.display = "none";
+    pokemon.style.display = "none"; //If the types of pokemon do not match the types selected by the user
   } else {
-    pokemon.style.display = "block";
+    pokemon.style.display = "block"; //In case the user deselects a tag, to show the hidden card again
   }
 }
 
 buttonsHeader.forEach((button) =>
   button.addEventListener("click", (event) => {
-    const buttonId = event.currentTarget.id;
+    const buttonId = event.currentTarget.id; //id of the selected button
+
+    //A pokemon cannot have more than two types
     if (tagList.length >= 2 && buttonId !== "all") {
-      alert("You can only choose two tags at a time.");
+      alert("You can only select 2 tags at a time.");
       return;
     }
 
-    pokemonCards = document.querySelectorAll(".pokemon");
+    pokemonCards = document.querySelectorAll(".pokemon"); //Selecting all pokemon cards
     if (buttonId === "all") {
-      tagList.splice(0);
+      //Show all cards
+      tagList.splice(0); //Empty the tagList
       tags.innerHTML = "";
-      tags.style.display = "none";
-      pokemonCards.forEach((pokemon) => noneDisplay(pokemon));
+      tags.style.display = "none"; //HTML element with class "tags should not be displayed"
+      pokemonCards.forEach((pokemon) => display(pokemon));
     } else {
       tags.innerHTML = "Selected tags:";
-      tagList.push(buttonId);
+      tagList.push(buttonId); //Include the selected tag in the tagList
 
+      //Show the selected tag on the website
       tagList.map((tag) => {
         let li = document.createElement("li");
         li.classList.add("nav-item");
@@ -101,57 +107,54 @@ buttonsHeader.forEach((button) =>
         `;
       });
 
-      cancelButtons = document.querySelectorAll(".cancel");
-      cancelTag(cancelButtons);
       tags.style.display = "flex";
 
-      pokemonCards.forEach((pokemon) => noneDisplay(pokemon));
+      cancelButtons = document.querySelectorAll(".cancel");
+      cancelTag(cancelButtons); //Add the event listener to the selected tags
+
+      pokemonCards.forEach((pokemon) => display(pokemon));
     }
   })
 );
 
 buttonShowMore.addEventListener("click", () => {
-  getData(start, start + 100);
-  if (start + 100 > numPokemon) {
+  getData(curr, curr + 100);
+  if (curr + 100 > numPokemon) {
+    //Maximum number of pokemons available reached
     buttonShowMore.disabled = true;
     buttonShowMore.textContent = "You have reached the end of the list";
   }
 });
 
 function cancelTag(cancelButtons) {
+  //Put the event listener in a function because "cancelButtons" element changes
   cancelButtons.forEach((cancel) =>
     cancel.addEventListener("click", (event) => {
       if (tagList.length === 1) {
+        //Remove the selected tags and show all cards
         tagList.splice(0);
         tags.innerHTML = "";
         tags.style.display = "none";
-        pokemonCards.forEach((pokemon) => noneDisplay(pokemon));
-      } else {
-        const buttonId = event.currentTarget.id;
+        pokemonCards.forEach((pokemon) => display(pokemon));
+      } else if (tagList.length === 2) {
+        const buttonId = event.currentTarget.id; //id of the button clicked
         if (tagList[0] === buttonId) {
-          tagList.shift();
+          tagList.shift(); //Remove first tag
         } else {
-          tagList.pop();
+          tagList.pop(); //Remove second tag
         }
-        tags.innerHTML = "Selected tags:";
 
-        tagList.map((tag) => {
-          let li = document.createElement("li");
-          li.classList.add("nav-item");
-          tags.appendChild(li);
-          li.innerHTML = `
-          <button id="${tag.toLowerCase()}" class="btn btn-header cancel ${
-            tag != "all" ? tag.toLowerCase() : ""
-          }">${tag}</button>
-        `;
-        });
+        //Remove the selected tag
+        tags.querySelector(`.${buttonId}`).remove();
 
         cancelButtons = document.querySelectorAll(".cancel");
-        cancelTag(cancelButtons);
-        pokemonCards.forEach((pokemon) => noneDisplay(pokemon));
+        cancelTag(cancelButtons); //Add the event listener to the new tags
+
+        pokemonCards.forEach((pokemon) => display(pokemon));
       }
     })
   );
 }
 
-getData(start, start + 100);
+//Fetch data for the first 100 pokemons automatically when the website is loaded
+getData(curr, curr + 100);
